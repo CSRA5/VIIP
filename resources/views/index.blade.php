f17b0eb02e4ae6cb18c34fee386a3babfc9f8c84
Christian Ramirez <c@dmark.pro>
2015-09-22 21:10:58 -0500

    AV Home

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts/layout')
+@section('title','Dmark-Agencia de Marketing Digital')
+@section('description','Somos una agencia creativa y apasionada por el Diseño Web, Inbound & Outbound Marketing')
+
+@section('content')
+
+
+@endsection
